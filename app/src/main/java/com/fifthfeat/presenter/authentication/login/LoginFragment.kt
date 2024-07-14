e2df1bd364d4a323c4299a55bf09943dfeb2411c
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-07-14 20:11:51 -0300

    Add add KSP and Hilt dependencies; Annotate relevant classes with Hilt annotations (e.g. `@AndroidEntryPoint`, `@Inject`)

--- app/src/main/java/com/fifthfeat/presenter/authentication/login/LoginFragment.kt
+++ app/src/main/java/com/fifthfeat/presenter/authentication/login/LoginFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.fifthfeat.databinding.FragmentLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
