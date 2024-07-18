d8049298a321ea8ffb5721a0ba9c148187d8a874
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-07-18 17:55:01 -0300

    Update Hilt version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("com.google.dagger.hilt.android") version "2.49" apply false
 }
