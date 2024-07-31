1a0ef308f87b9df45f9d677244c6d433b68babbd
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-07-30 22:00:49 -0300

    Optimize imports

--- app/src/main/java/com/fifthfeat/presenter/authentication/activity/AuthActivity.kt
+++ app/src/main/java/com/fifthfeat/presenter/authentication/activity/AuthActivity.kt
@@ -1,19 +1,9 @@
 package com.fifthfeat.presenter.authentication.activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.fifthfeat.databinding.ActivityAuthBinding
-import com.fifthfeat.presenter.authentication.home.AuthHomeFragment
-import com.fifthfeat.util.OauthKey.DEFAULT_WEB_CLIENT_ID
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
