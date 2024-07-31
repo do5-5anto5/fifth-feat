40ee82fa956fe85829f629c4cc58987eedcbcd76
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-07-30 22:02:42 -0300

    Implement Google authentication into AuthHomeFragment, RegisterFragment and LoginFragment

--- app/src/main/java/com/fifthfeat/presenter/authentication/home/AuthHomeFragment.kt
+++ app/src/main/java/com/fifthfeat/presenter/authentication/home/AuthHomeFragment.kt
@@ -1,21 +1,31 @@
 package com.fifthfeat.presenter.authentication.home
 
-import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.fifthfeat.R
 import com.fifthfeat.databinding.FragmentAuthHomeBinding
+import com.fifthfeat.util.FirebaseHelper.Companion.getAuth
+import com.fifthfeat.util.OauthKey.DEFAULT_WEB_CLIENT_ID
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.GoogleAuthProvider
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AuthHomeFragment : Fragment() {
     private var _binding: FragmentAuthHomeBinding? = null
     private val binding get() = _binding!!
-    private var listener: GoogleLoginClickListener? = null
+
+    private val requestCodeSignIn: Int = 9001
+    private var googleSignInClient: GoogleSignInClient? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,28 +35,24 @@ class AuthHomeFragment : Fragment() {
         return binding.root
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is GoogleLoginClickListener) {
-            listener = context
-        } else {
-            throw RuntimeException("$context must implement OnButtonClickedListener")
-        }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initListeners()
-    }
 
-    interface GoogleLoginClickListener {
-        fun onGoogleLoginClick()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(DEFAULT_WEB_CLIENT_ID)
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
     }
 
     private fun initListeners() {
         with(binding) {
             btnGoogleLogin.setOnClickListener {
-                listener?.onGoogleLoginClick() // Notify the activity about the click
+                signInWithGoogle(
+                )
             }
             btnPasswordLogin.setOnClickListener {
                 findNavController().navigate(R.id.action_authHomeFragment_to_loginFragment)
@@ -57,9 +63,37 @@ class AuthHomeFragment : Fragment() {
         }
     }
 
-    override fun onDetach() {
-        super.onDetach()
-        listener = null // Evita vazamentos de memória
+    private fun signInWithGoogle() {
+        val signInIntent = googleSignInClient!!.signInIntent
+        startActivityForResult(signInIntent, requestCodeSignIn)
+    }
+
+    @Deprecated("This method has been deprecated")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account.idToken)
+            } catch (e: ApiException) {
+                Toast.makeText(requireContext(), "login failed", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String?) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        getAuth().signInWithCredential(credential)
+            .addOnCompleteListener(
+                requireActivity()
+            ) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(requireContext(), "SUCCESS", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(requireContext(), "login failed", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
     }
 
     override fun onDestroyView() {
