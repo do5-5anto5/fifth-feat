e833e73a6eb1a311ead57c0c37e5d6e1a3910931
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-07-30 21:48:05 -0300

    remove authentication responsability from AuthActivity

--- app/src/main/java/com/fifthfeat/presenter/authentication/activity/AuthActivity.kt
+++ app/src/main/java/com/fifthfeat/presenter/authentication/activity/AuthActivity.kt
@@ -17,66 +17,14 @@ import com.google.firebase.auth.GoogleAuthProvider
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class AuthActivity : AppCompatActivity(), AuthHomeFragment.GoogleLoginClickListener {
+class AuthActivity : AppCompatActivity(){
 
     private lateinit var binding: ActivityAuthBinding
-    private val RC_SIGN_IN: Int = 9001
-    private var googleSignInClient: GoogleSignInClient? = null
-    private var auth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        auth = FirebaseAuth.getInstance()
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(DEFAULT_WEB_CLIENT_ID)
-            .requestEmail()
-            .build()
-
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
-
-    override fun onGoogleLoginClick() {
-        signIn()
-    }
-
-    private fun signIn() {
-        val signInIntent = googleSignInClient!!.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
-    }
-
-    @Deprecated("This method has been deprecated")
-    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                val account = task.getResult(ApiException::class.java)
-                firebaseAuth(account.idToken)
-            } catch (e: ApiException) {
-                Toast.makeText(this, "login failed", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    private fun firebaseAuth(idToken: String?) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        auth!!.signInWithCredential(credential)
-            .addOnCompleteListener(
-                this
-            ) { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(this, "SUCCESS", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, "login failed", Toast.LENGTH_SHORT)
-                        .show()
-                }
-            }
-    }
-
 }
