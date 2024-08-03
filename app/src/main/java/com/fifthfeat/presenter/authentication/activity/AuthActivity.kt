7a9f28ae4d62d1ab27cc3858ba0cc550d3ac4485
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-08-02 23:54:12 -0300

    Check authentication on AuthActivity

--- app/src/main/java/com/fifthfeat/presenter/authentication/activity/AuthActivity.kt
+++ app/src/main/java/com/fifthfeat/presenter/authentication/activity/AuthActivity.kt
@@ -1,9 +1,12 @@
 package com.fifthfeat.presenter.authentication.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.fifthfeat.databinding.ActivityAuthBinding
+import com.fifthfeat.presenter.main.activity.MainActivity
+import com.fifthfeat.util.FirebaseHelper.Companion.getCurrentUser
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,5 +19,13 @@ class AuthActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        verifyAuth()
+    }
+
+    private fun verifyAuth() {
+        if (getCurrentUser()) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
